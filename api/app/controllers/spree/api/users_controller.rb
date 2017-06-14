b8d721754c572a18f05b2cd85ca7083614d525c3
KenZ <ken.z@tappr.io>
2017-06-14 10:08:30 +1000

    [api] add support for auth

--- api/app/controllers/spree/api/users_controller.rb
+++ api/app/controllers/spree/api/users_controller.rb
@@ -1,4 +1,29 @@
 class Spree::Api::UsersController < Spree::Api::ResourceController
+  before_action :authenticate_user, :except => [:sign_up, :sign_in]
+  prepend_before_action :allow_params_authentication!, only: :sign_in
+
+  def sign_up
+    user_params = params.require(:spree_user).permit :email, :password, :password_confirmation
+    @user = Spree.user_class.new user_params
+    if @user.save
+      @user.generate_spree_api_key!
+      render 'show'
+    else
+      invalid_resource! @user
+    end
+  end
+
+  def sign_in
+    user_params = params.require(:spree_user).permit :email, :password
+    if warden.authenticate scope: :spree_user
+      @user = warden.user scope: :spree_user
+      @user.generate_spree_api_key! if @user.spree_api_key.blank?
+      render 'auth'
+    else
+      render 'invalid', status: 401
+    end
+  end
+
   private
 
   attr_reader :user
